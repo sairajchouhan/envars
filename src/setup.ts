62a3bb322ab098f4f4d440d01eac8e75e91830b6
sairaj <sairaj2119@gmail.com>
2022-05-15 13:09:54 +0530

    convert setup.js to setup.ts

---
+++ src/setup.ts
@@ -0,0 +1,25 @@
+import { writeFile, mkdir, readdir } from 'fs/promises'
+import { join } from 'path'
+
+const home =
+  process.platform === 'win32' ? process.env.USERPROFILE : process.env.HOME
+
+const data_dir_path = join(home || '~/', '.envars')
+
+export const setup = async () => {
+  try {
+    const resp = await readdir(data_dir_path)
+
+    if (!resp.includes('data.json')) {
+      await writeFile(join(data_dir_path, 'data.json'), '')
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    if (err.code === 'ENOENT') {
+      await mkdir(data_dir_path)
+      await writeFile(join(data_dir_path, 'data.json'), '')
+    }
+  }
+}
+
+setup()
